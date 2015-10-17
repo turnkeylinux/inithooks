0d50c286b55ee0d2fab1d29dbeadbbde799cfe89
Anton Pyrogovskyi <anton@turnkeylinux.org>
2015-10-17 04:58:01 +0000

    Completely silence curl

--- bin/secalerts.sh
+++ bin/secalerts.sh
@@ -61,7 +61,7 @@ enable_security_alerts() {
 curl https://hub.turnkeylinux.org/api/server/secalerts/ \\
     -d email="$email" \\
     -d turnkey_version="$turnkey_version" \\
-    --silent --fail --output /dev/null
+    --silent --fail --output /dev/null 2>&1 > /dev/null
 
 rm -f \$0
 EOF
