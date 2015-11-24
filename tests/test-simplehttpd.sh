8f32ac986c8cb6d82b97b802198fbc5a1aaee2d4
Liraz Siri <liraz@turnkeylinux.org>
2015-11-24 14:55:40 +0200

    added simplehttpd test case

---
+++ tests/test-simplehttpd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+src=$(dirname $0)/../
+
+echo "Initfence htdocs @ http://127.0.0.1:60080/ https://127.0.0.1:60443/"
+$src/bin/simplehttpd.py $src/turnkey-init-fence/htdocs 60080 60443 $src/tests/cert.pem $src/tests/cert.key
